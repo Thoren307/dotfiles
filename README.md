ed4a59504b158e1c7acf90394b8f254baf4b1b4b
Thoren307 <132410081+Thoren307@users.noreply.github.com>
2025-05-03 01:44:45 -0400

    Added my dotfiles and README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotfiles
 My linux dotfiles
+
+Specs:
+OS: Arch (Planning to hop to cachy os)
+WM: Hyprland
+Boot: Systemd (Might switch to rEFInd)
---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+alias neofetch='fastfetch'
+fastfetch
+starship init fish | source
+end
